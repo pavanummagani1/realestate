2a708d8631c33fdddda63bc8b6c7a44a3033f71c
PavanUmmagani07 <pavankalyanU@outlook.com>
2025-06-25 16:30:51 +0530

    frontend

--- frontend/src/pages/CreateListing.jsx
+++ frontend/src/pages/CreateListing.jsx
@@ -26,6 +26,7 @@ export default function CreateListing() {
     offer: false,
     parking: false,
     furnished: false,
+    email:true,
   });
   const [imageUploadError, setImageUploadError] = useState(false);
   const [uploading, setUploading] = useState(false);
